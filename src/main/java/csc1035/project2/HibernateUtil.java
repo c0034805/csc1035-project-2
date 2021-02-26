1d62b4723867ec063baff4cfd625507bfbfa1115
Matthew Taylor (UG) <c0007723@newcastle.ac.uk>
2021-02-26 11:28:08 +0000

    Format HibernateUtil

--- src/main/java/csc1035/project2/HibernateUtil.java
+++ src/main/java/csc1035/project2/HibernateUtil.java
@@ -3,16 +3,13 @@ package csc1035.project2;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-
 public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
-
         try {
-
             return new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex){
+        } catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
@@ -22,7 +19,7 @@ public class HibernateUtil {
         return sessionFactory;
     }
 
-    public static void shutdown(){
+    public static void shutdown() {
         getSessionFactory().close();
     }
 }
