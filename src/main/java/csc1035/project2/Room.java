ef2c340b66a6332b5a3961b1b26fe95827790656
Matthew Taylor (UG) <c0007723@newcastle.ac.uk>
2021-03-01 15:59:29 +0000

    Add Room class

---
+++ src/main/java/csc1035/project2/Room.java
@@ -0,0 +1,65 @@
+package csc1035.project2;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Rooms")
+public class Room {
+    @Id
+    @Column(name = "Room_Number")
+    private int num;
+
+    @Column(name = "Type")
+    // TODO: Stop using string IDs for things like this. This should be an enum or a class.
+    private String type;
+
+    @Column(name = "Max_Capacity")
+    private int cap;
+
+    @Column(name = "Social_Distancing_Capacity")
+    private int sd_cap;
+
+    public Room() {}
+
+    public Room(int num, String type, int cap, int sd_cap) {
+        this.num = num;
+        this.type = type;
+        this.cap = cap;
+        this.sd_cap = sd_cap;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCap() {
+        return cap;
+    }
+
+    public int getSd_cap() {
+        return sd_cap;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCap(int cap) {
+        this.cap = cap;
+    }
+
+    public void setSd_cap(int sd_cap) {
+        this.sd_cap = sd_cap;
+    }
+}
