14dd2c25e8fb8b466cfbd2c65871f615012a5855
Lucas Toner <lucasgeorgetoner@hotmail.com>
2021-03-03 12:09:28 +0000

    Updated RoomNo to string

--- src/main/java/csc1035/project2/Room.java
+++ src/main/java/csc1035/project2/Room.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 public class Room {
     @Id
     @Column(name = "Room_Number")
-    private int num;
+    private String num;
 
     @Column(name = "Type")
     // TODO: Stop using string IDs for things like this. This should be an enum or a class.
@@ -24,14 +24,14 @@ public class Room {
 
     public Room() {}
 
-    public Room(int num, String type, int cap, int sd_cap) {
+    public Room(String num, String type, int cap, int sd_cap) {
         this.num = num;
         this.type = type;
         this.cap = cap;
         this.sd_cap = sd_cap;
     }
 
-    public int getNum() {
+    public String getNum() {
         return num;
     }
 
@@ -47,7 +47,7 @@ public class Room {
         return sd_cap;
     }
 
-    public void setNum(int num) {
+    public void setNum(String num) {
         this.num = num;
     }
 
