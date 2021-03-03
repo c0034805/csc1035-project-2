0ec2e06fa32a4526f52905f9e7641f0d2aca4404
c0034805 <c0034805@newcastle.ac.uk>
2021-03-03 14:59:11 +0200

    Add Modules class

---
+++ src/main/java/csc1035/project2/Modules.java
@@ -0,0 +1,62 @@
+package csc1035.project2;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Modules")
+public class Modules {
+    @Id
+    @Column(name = "ID", nullable = false)
+    private String id;
+
+    @Column(name = "Name", nullable = false)
+    private String name;
+
+    @Column(name = "Credits", nullable = false)
+    private int credits;
+
+    @Column(name = "Weeks", nullable = false)
+    private int weeks;
+
+    public Modules(String id, String name, int credits, int weeks) {
+        this.id = id;
+        this.name = name;
+        this.credits = credits;
+        this.weeks = weeks;
+    }
+
+    public Modules() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public int getWeeks() {
+        return weeks;
+    }
+
+    public void setWeeks(int weeks) {
+        this.weeks = weeks;
+    }
+}
