fa83bfe932d26b8ff226759fd267d18cc8ef5b96
Lucas Toner <lucasgeorgetoner@hotmail.com>
2021-03-03 20:31:30 +0000

    Added StudentReservation in RoomHandlerTest

--- src/main/java/csc1035/test_suite/RoomHandlerTest.java
+++ src/main/java/csc1035/test_suite/RoomHandlerTest.java
@@ -1,11 +1,11 @@
 package csc1035.test_suite;
 
-import csc1035.project2.Room;
-import csc1035.project2.RoomHandler;
+import csc1035.project2.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,17 +14,18 @@ import java.util.List;
 public class RoomHandlerTest {
     //these tests rely on the database containing the sample data
     RoomHandler handler;
+    IController controller;
 
     @BeforeEach
     void setUp() {
-        RoomHandler handler = new RoomHandler();
+        handler = new RoomHandler();
         handler.clearBookings();
+        controller = new Controller();
     }
 
     @Test
     public void getRoomsReturnsAll() {
         boolean checker = true;
-        RoomHandler handler = new RoomHandler();
 
         String[] idsArr = {"0.379","0.365","1.846","1.862","2.484","2.019","3.473","3.940","4.178","4.550"};
         List<String> ids = Arrays.asList(idsArr);
@@ -38,6 +39,25 @@ public class RoomHandlerTest {
         }
         checker =  checker && ids.size() == rooms.size();
         Assertions.assertTrue(checker);
+    }
+
+    public void studentReservation(){
+        Students s = (Students) controller.getById(Students.class,216906208);
+        Room r = (Room) controller.getById(Room.class,"0.379");
+
+        Timestamp start = new Timestamp(2021, 6, 22,
+                12, 0,0,0);
+        Timestamp end = new Timestamp(2021, 6, 22,
+                13, 0,0,0);
+
+        handler.reserveRoomStudent(s,r,start,end);
+    }
+
+    @Test
+    public void reserveRoomStudentReservesRoom(){
+        studentReservation();
+        //need to figure out how the booking ID gets made
+
     }
 
 
