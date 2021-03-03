8939eb7a0f9be1667727e46bbb683f19f4197851
c0034805 <c0034805@newcastle.ac.uk>
2021-03-03 15:25:52 +0200

    Add Booking class

---
+++ src/main/java/csc1035/project2/Booking.java
@@ -0,0 +1,63 @@
+package csc1035.project2;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "Booking")
+public class Booking {
+    @Id
+    @Column(name = "Booking_ID", nullable = false)
+    private int id;
+
+    @Column(name = "Room_Number", nullable = false)
+    private int num;
+
+    @Column(name = "Start", nullable = false)
+    private Timestamp start;
+
+    @Column(name = "End", nullable = false)
+    private Timestamp end;
+
+    public Booking(int id, int num, Timestamp start, Timestamp end) {
+        this.id = id;
+        this.num = num;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Booking() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public Timestamp getStart() {
+        return start;
+    }
+
+    public void setStart(Timestamp start) {
+        this.start = start;
+    }
+
+    public Timestamp getEnd() {
+        return end;
+    }
+
+    public void setEnd(Timestamp end) {
+        this.end = end;
+    }
+}
