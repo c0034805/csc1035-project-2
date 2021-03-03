8b56beaac5ed97e3e9ab25bcac7dec437522fd66
c0034805 <c0034805@newcastle.ac.uk>
2021-03-03 15:11:24 +0200

    Add staff class

---
+++ src/main/java/csc1035/project2/Staff.java
@@ -0,0 +1,50 @@
+package csc1035.project2;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Staff")
+public class Staff{
+    @Id
+    @Column(name = "ID", nullable = false)
+    private String id;
+
+    @Column(name = "First_Name", nullable = false)
+    private String firstname;
+
+    @Column(name = "Last_Name", nullable = false)
+    private String lastname;
+
+    public Staff(String id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Staff() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
