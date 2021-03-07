f4874687fbc32b36117ba1ca781211a93942d053
c0034805 <c0034805@newcastle.ac.uk>
2021-03-07 11:12:29 +0200

    Add javadoc for StaffBooking class

--- src/main/java/csc1035/project2/StaffBooking.java
+++ src/main/java/csc1035/project2/StaffBooking.java
@@ -2,6 +2,18 @@ package csc1035.project2;
 
 import javax.persistence.*;
 
+/**
+ * A persistent class made to handle the Staff_Booking table, which
+ * links the Staff table with the Booking table. It represents the
+ * bookings made by individual staff members.
+ *
+ * The table contains a booking ID and a Staff ID.
+ *
+ * This Class connects the attributes to their respective columns in the
+ * table, makes the necessary relationships with other tables and
+ * contains the relevant getter and setter methods.
+ * @author Stefanos Larkou
+ */
 @Entity
 @Table(name = "Staff_Booking")
 public class StaffBooking {
@@ -19,11 +31,20 @@ public class StaffBooking {
     @JoinColumn(nullable = false)
     private Staff staff;
 
+    /**
+     * The constructor that connects the parameter values with the field
+     * variables.
+     * @param bid The booking ID.
+     * @param sid The staff member's ID.
+     */
     public StaffBooking(int bid, String sid) {
         this.bid = bid;
         this.sid = sid;
     }
 
+    /**
+     * The default constructor for Hibernate.
+     */
     public StaffBooking() {
     }
 
